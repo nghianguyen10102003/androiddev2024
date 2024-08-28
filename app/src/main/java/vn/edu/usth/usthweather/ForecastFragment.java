f5387d67c3edb2c4c7e2bfda6a05b281aab9a858
nghia nguyen <nghia@gmail.com>
2024-08-28 16:05:59 +0700

    prac 4

--- app/src/main/java/vn/edu/usth/usthweather/ForecastFragment.java
+++ app/src/main/java/vn/edu/usth/usthweather/ForecastFragment.java
@@ -7,6 +7,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -58,9 +61,20 @@ public class ForecastFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_forecast,container,false);
-        int color = 0x20FF0000;
-        view.setBackgroundColor(color);
+        TextView day = new TextView(getContext());
+        day.setText("thursday");
+
+        ImageView img = new ImageView(getContext());
+        img.setImageResource(R.drawable.sun);
+
+        View v = new View(getContext());
+        v.setBackgroundColor(0x20FF0000);
+
+        LinearLayout view = new LinearLayout(getContext());
+        view.setOrientation(LinearLayout.VERTICAL);
+        view.addView(day);
+        view.addView(img);
+        view.addView(v);
         return view;
     }
 }
