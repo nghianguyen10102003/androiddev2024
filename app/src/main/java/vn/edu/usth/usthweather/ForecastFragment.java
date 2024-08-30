445e16be408f6fec6558b5e4d68bb97bf707560e
nghia nguyen <nghia@gmail.com>
2024-08-30 15:36:11 +0700

    prac 5

--- app/src/main/java/vn/edu/usth/usthweather/ForecastFragment.java
+++ app/src/main/java/vn/edu/usth/usthweather/ForecastFragment.java
@@ -61,20 +61,21 @@ public class ForecastFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        TextView day = new TextView(getContext());
-        day.setText("thursday");
-
-        ImageView img = new ImageView(getContext());
-        img.setImageResource(R.drawable.sun);
-
-        View v = new View(getContext());
-        v.setBackgroundColor(0x20FF0000);
-
-        LinearLayout view = new LinearLayout(getContext());
-        view.setOrientation(LinearLayout.VERTICAL);
-        view.addView(day);
-        view.addView(img);
-        view.addView(v);
-        return view;
+//        TextView day = new TextView(getContext());
+//        day.setText("thursday");
+//
+//        ImageView img = new ImageView(getContext());
+//        img.setImageResource(R.drawable.sun);
+//
+//        View v = new View(getContext());
+//        v.setBackgroundColor(0x20FF0000);
+//
+//        LinearLayout view = new LinearLayout(getContext());
+//        view.setOrientation(LinearLayout.VERTICAL);
+//        view.addView(day);
+//        view.addView(img);
+//        view.addView(v);
+//        return view;
+        return inflater.inflate(R.layout.fragment_forecast, container, false);
     }
 }
---
+++ deap_[r2.py
@@ -0,0 +1,4 @@
+import matplotlib.pyplot as plt
+x=[1,2,3]
+y==x^2
+plt.plot(x,y)
